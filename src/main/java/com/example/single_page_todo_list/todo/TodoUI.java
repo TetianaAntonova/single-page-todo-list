cf7777e9c7db376129b89652ed690e3ec0b762fa
Tatyana <taa@anadeainc.com>
2025-01-31 20:55:30 +0200

    show all records

--- src/main/java/com/example/single_page_todo_list/todo/TodoUI.java
+++ src/main/java/com/example/single_page_todo_list/todo/TodoUI.java
@@ -1,12 +1,26 @@
 package com.example.single_page_todo_list.todo;
 
+import com.example.single_page_todo_list.todo.model.TodoRecord;
+import com.example.single_page_todo_list.todo.storage.InMemoryRepository;
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route(value = "todo")
-public class TodoUI extends VerticalLayout {
+public class TodoUI extends VerticalLayout implements BeforeEnterObserver {
+
+    private final InMemoryRepository repository;
+
+    public TodoUI(InMemoryRepository repository) {
+        this.repository = repository;
+    }
+
+    Grid<TodoRecord> grid;
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
@@ -14,6 +28,24 @@ public class TodoUI extends VerticalLayout {
 
         H2 title = new H2("TodoList ...");
         add(title);
+
+        grid = new Grid();
+        grid.addColumn(TodoRecord::getTitle).setHeader("Title");
+        grid.addColumn(TodoRecord::getDescription).setHeader("Description");
+        grid.addColumn(TodoRecord::getAuthor).setHeader("Author");
+        grid.addColumn(TodoRecord::getCreatedAt).setHeader("Created At");
+        grid.addColumn(TodoRecord::isClosed).setHeader("Closed");
+        grid.setSelectionMode(Grid.SelectionMode.MULTI);
+        setItems();
+        add(grid);
+    }
+
+    private void setItems(){
+        grid.setItems(repository.getRecords());
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
 
     }
 }
--- src/main/java/com/example/single_page_todo_list/todo/storage/InMemoryRepository.java
+++ src/main/java/com/example/single_page_todo_list/todo/storage/InMemoryRepository.java
@@ -1,4 +1,36 @@
 package com.example.single_page_todo_list.todo.storage;
 
+import com.example.single_page_todo_list.todo.model.TodoRecord;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class InMemoryRepository {
+
+    private final List<TodoRecord> records = new ArrayList<>();
+
+    @PostConstruct
+    void setup() {
+        for (int i = 1; i < 10; i++) {
+            add(TodoRecord.builder()
+                    .title("# " + i)
+                    .description("Buy salad")
+                    .author("John Doe")
+                    .createdAt(LocalDateTime.now())
+                    .closed(true)
+                    .build());
+        }
+    }
+
+    public void add(TodoRecord record) {
+        records.add(record);
+    }
+
+    public List<TodoRecord> getRecords() {
+        return records;
+    }
 }
